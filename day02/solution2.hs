d8d6ae95f03ee8c19b0b24c810a171b5321353c5
Andy Peterson <apeterson@cafemedia.com>
2025-12-02 22:09:01 -0500

    cleanup day 2

--- day02/solution2.hs
+++ day02/solution2.hs
@@ -1,10 +1,7 @@
-import Distribution.Simple.Utils (xargs, isInfixOf)
-import Data.List (intersect)
 main = do
   ranges <- readInputFile "input2.txt"
   let result = concatMap (filter (not . isValid)) ranges
   print $ sum result
-
 
 readInputFile fileName = do
   contents <- readFile fileName
@@ -22,32 +19,11 @@ split delim str = case break (== delim) str of
 isValid :: Integer -> Bool
 isValid id = not $ hasAtLeastTwoRepeatingSequences $ show id
 
-cutInHalfAndSwap :: String -> String
-cutInHalfAndSwap x =
-  let i = length x `div` 2
-      a = take i x
-      b = reverse $ take i $ reverse x
-      res = b ++ a
-  in res
-
 hasAtLeastTwoRepeatingSequences :: String -> Bool
 hasAtLeastTwoRepeatingSequences x =
   let combinations = generateCombinations x
       stringsToCheck = foldl (\acc y -> if rem (length x) (length y) == 0 then acc ++ [concat (replicate (length x `div` length y) y)]  else acc ) [] combinations
   in elem x stringsToCheck
 
--- generateCombinations :: String -> [String]
--- generateCombinations x
---   | null x = []
---   | otherwise = map (`take` x) [1..length x] ++ generateCombinations (tail x)
-
 generateCombinations :: String -> [String]
 generateCombinations x = map (`take` x) [1..length x - 1]
-
-getSequences :: Int -> String -> [String]
-getSequences len x
-  | length x == len = [x]
-  | otherwise = take len x : getSequences len (tail x)
-
-determineStreak :: String -> [String] -> Int
-determineStreak x combinations = fst $ foldl (\acc y -> if y == x then (fst acc + 1, length x) else if snd acc > 0 then (fst acc, snd acc - 1) else (0,0) ) (0,0) combinations
